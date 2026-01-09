9e8bdd4a0b074ca6ce6d2ca9f30529af7e8bd928
duclong27 <longofficial27@gmail.com>
2026-01-09 10:58:48 +0700

    Fixxx

--- shop-bff/controller/authController.js
+++ shop-bff/controller/authController.js
@@ -1,22 +1,45 @@
+// import { loginCustomerService, registerCustomerService } from "../service/customer/authService.js";
+
+
+
+// export async function registerCustomerController(req, res, next) {
+//   try {
+//     const result = await registerCustomerService(req.body);
+//     res.json(result);
+//   } catch (err) {
+//     next(err);
+//   }
+// }
+
+
+// export async function loginCustomerController(req, res, next) {
+//   try {
+//     const result = await loginCustomerService(req.body);
+//     res.json(result);
+//   } catch (err) {
+//     next(err);
+//   }
+// }
+
+// new auth controller for demo
+
 import { loginCustomerService, registerCustomerService } from "../service/customer/authService.js";
-
-
+import { passCoreError } from "../utils/errorMapper.js";
 
 export async function registerCustomerController(req, res, next) {
   try {
     const result = await registerCustomerService(req.body);
     res.json(result);
   } catch (err) {
-    next(err);
+    return passCoreError(res, err);
   }
 }
-
 
 export async function loginCustomerController(req, res, next) {
   try {
     const result = await loginCustomerService(req.body);
     res.json(result);
   } catch (err) {
-    next(err);
+    return passCoreError(res, err);
   }
 }
